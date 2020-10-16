ad14760e83862f7ad33787b3ba8f215b325ff242
sumit0221 <sumitsc.work@gmail.com>
2020-10-16 22:08:41 +0530

    Sorting Using Merge Sort

---
+++ Assignment Day 4(16-10-2020)/MergeSort.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+ using namespace std;
+
+void Merge(int *a, int low, int high, int mid)
+{
+	int i, j, k, temp[high-low+1];
+	i = low;
+	k = 0;
+	j = mid + 1;
+
+	while (i <= mid && j <= high)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+
+
+	for (i = low; i <= high; i++)
+	{
+		a[i] = temp[i-low];
+	}
+}
+
+
+void MergeSort(int a[], int low, int high)
+{
+	int mid;
+	if (low < high)
+	{
+		mid=(low+high)/2;
+		MergeSort(a, low, mid);
+		MergeSort(a, mid+1, high);
+
+		Merge(a, low, high, mid);
+	}
+}
+
+int main()
+{
+    cout<<"-----------------Merge Sort------------------\n\n";
+	int n, i;
+	cout<<"\nEnter the no. of elements to be entered in array using merge sort: \n";
+	cin>>n;
+    cout << "\nArray Of Size"<<" " <<n<<" "<<"Created.\n\n";
+	int arr[n];
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Enter element "<<i+1<<": ";
+		cin>>arr[i];
+	}
+    cout<<"\nUnsortedSorted Data : \n";
+	for (i = 0; i < n; i++)
+        cout<<arr[i]<<" ";
+        cout << endl;
+
+	MergeSort(arr, 0, n-1);
+
+	cout<<"\nSorted Data :\n";
+	for (i = 0; i < n; i++)
+        cout<<arr[i]<<" ";
+        cout << endl;
+
+	return 0;
+}
